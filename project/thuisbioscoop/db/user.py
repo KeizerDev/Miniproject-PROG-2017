9b50674717739730c02d5d25d5a08a6ad289f17f
Robert-Jan Keizer <robertjankeizer@gmail.com>
2017-11-06 11:47:40 +0100

    Basic structure

---
+++ project/thuisbioscoop/db/user.py
@@ -0,0 +1,15 @@
+import sqlobject
+from .connection import conn
+
+
+class User(sqlobject.SQLObject):
+    _connection = conn
+
+    code = sqlobject.StringCol(length=14, unique=True)
+    firstName = sqlobject.StringCol()
+    mi = sqlobject.StringCol(length=1, default=None)
+    lastName = sqlobject.StringCol()
+    date = sqlobject.DateTimeCol(default=None)
+
+
+User.createTable(ifNotExists=True)
