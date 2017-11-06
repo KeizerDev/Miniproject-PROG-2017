c7b1326c7f33356c38593633f48225ae58da1cc3
eline <eline.nube@student.hu.nl>
2017-11-06 17:30:21 +0100

    Basis Gui gemaakt

--- project/main.py
+++ project/main.py
@@ -1,12 +1,20 @@
 import tkinter as tk
 
-class Navbar(tk.Frame):
-    pass
+class start():
+    def __init__(self, root):
+        global frame_start
+        frame_start = tk.Frame(root, background="#AA0203")
 
+        welkom = tk.Label(frame_start, text="Welkom, maak uw keuze:", foreground="white", background="#AA0203", height=5, font=10)
+        button1 = tk.Button(frame_start, text="Ik ben aanbieder", height=3, width=25)
+        button2 = tk.Button(frame_start, text="Ik ben bezoeker", height=3, width=25)
+        button3 = tk.Button(frame_start, text="Ik wil publieke informatie zien", height=3, width=25)
 
-class Toolbar(tk.Frame):
-    pass
-
+        frame_start.pack(fill="both", expand=True)
+        welkom.pack()
+        button1.pack()
+        button2.pack()
+        button3.pack()
 
 class Statusbar(tk.Frame):
     pass
@@ -15,20 +23,17 @@ class Statusbar(tk.Frame):
 class Main(tk.Frame):
     pass
 
-
 class MainApplication(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.statusbar = Statusbar(self)
-        self.toolbar = Toolbar(self)
-        self.navbar = Navbar(self)
+        self.start = start(self)
         self.main = Main(self)
 
-        self.statusbar.pack(side="bottom", fill="x")
-        self.toolbar.pack(side="top", fill="x")
-        self.navbar.pack(side="left", fill="y")
-        self.main.pack(side="right", fill="both", expand=True)
-
+        # Insert
+        #user3 = User(code="13", firstName="fkd", mi="fl", lastName="asld")
+        #selectUser = User.selectBy(code="123")
+        #print(selectUser)
 
 if __name__ == "__main__":
     root = tk.Tk()
