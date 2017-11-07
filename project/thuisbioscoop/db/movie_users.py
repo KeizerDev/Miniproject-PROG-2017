987a87320200e6fd68a1e672eab5d672d48b9eec
Jurrian120 <jurrian.hendriks@student.hu.nl>
2017-11-07 14:58:03 +0100

    db update

--- project/thuisbioscoop/db/movie_users.py
+++ project/thuisbioscoop/db/movie_users.py
@@ -1,10 +1,14 @@
 import sqlobject
+
+from project.thuisbioscoop.db.user import User
 from .connection import conn
 
 
-class Film_Users(sqlobject.SQLObject):
+class MovieUsers(sqlobject.SQLObject):
     _connection = conn
     _connection.debug = True
+    user_id = ForeignKey("User", dbName=User.q.id)
+    movie_supplier_id = ForeignKey("MovieSupplier", dbName=MovieSupplier.q.id)
 
 
-Film.createTable(ifNotExists=True)
+MovieUsers.createTable(ifNotExists=True)
