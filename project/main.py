c6df7a97297ca7cdfe407bc004c75be079ebd50a
Yinka Adetunji <yinka.adetunji@hva.nl>
2017-11-07 11:13:06 +0100

    Upgedate, alle functies in de class gezet

--- project/main.py
+++ project/main.py
@@ -1,48 +1,50 @@
 import tkinter as tk
 
-class start():
+class Start():
     def __init__(self, master):
-        frame_start = tk.Frame(master, background="#AA0203")
+        self.master = master
+        self.frame_start = tk.Frame(master, background="#AA0203")
+        self.welkom = tk.Label(self.frame_start, text="Welkom, maak uw keuze:", foreground="white", background="#AA0203", height=5, font=10)
+        self.button1 = tk.Button(self.frame_start, text="Ik ben aanbieder", height=3, width=25, command=self.go_to_supplier)
+        self.button2 = tk.Button(self.frame_start, text="Ik ben bezoeker", height=3, width=25, command=self.go_to_visitor)
+        self.button3 = tk.Button(self.frame_start, text="Ik wil publieke informatie zien", height=3, width=25, command=self.go_to_public)
 
-        def show_supplier():
-            frame_start.pack_forget()
-            frame_supplier = tk.Frame(master, background="blue")
-            frame_supplier.pack(fill="both",expand=True)
+        self.frame_start.pack(fill="both", expand=True)
+        self.welkom.pack()
+        self.button1.pack()
+        self.button2.pack()
+        self.button3.pack()
 
-        def go_to_supplier():
-            show_supplier()
+    def show_supplier(self):
+        self.frame_start.pack_forget()
+        self.frame_supplier = tk.Frame(self.master, background="blue")
+        self.frame_supplier.pack(fill="both",expand=True)
 
-        def show_visitor():
-            frame_start.pack_forget()
-            frame_visitor = tk.Frame(master, background="green")
-            frame_visitor.pack(fill="both",expand=True)
+    def go_to_supplier(self):
+        self.show_supplier()
 
-        def go_to_visitor():
-            show_visitor()
+    def show_visitor(self):
+        self.frame_start.pack_forget()
+        self.frame_visitor = tk.Frame(self.master, background="green")
+        self.frame_visitor.pack(fill="both",expand=True)
 
-        def show_public():
-            frame_start.pack_forget()
-            frame_public = tk.Frame(master, background="black")
-            frame_public.pack(fill="both",expand=True)
+    def go_to_visitor(self):
+        self.show_visitor()
 
-        def go_to_public():
-            show_public()
+    def show_public(self):
+        self.frame_start.pack_forget()
+        self.frame_public = tk.Frame(self.master, background="black")
+        self.frame_public.pack(fill="both",expand=True)
 
-        welkom = tk.Label(frame_start, text="Welkom, maak uw keuze:", foreground="white", background="#AA0203", height=5, font=10)
-        button1 = tk.Button(frame_start, text="Ik ben aanbieder", height=3, width=25, command=go_to_supplier)
-        button2 = tk.Button(frame_start, text="Ik ben bezoeker", height=3, width=25, command=go_to_visitor)
-        button3 = tk.Button(frame_start, text="Ik wil publieke informatie zien", height=3, width=25, command=go_to_public)
+    def go_to_public(self):
+        self.show_public()
 
-        frame_start.pack(fill="both", expand=True)
-        welkom.pack()
-        button1.pack()
-        button2.pack()
-        button3.pack()
+       
 
 class MainApplication(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
-        self.start = start(self)
+        self.start = Start(self)
 
         # Insert
         #user3 = User(code="13", firstName="fkd", mi="fl", lastName="asld")
