1c7548bf3a56dc463987f4a6f742f1edef090a21
Steven <steven.toet@student.hu.nl>
2017-11-07 14:47:30 +0100

    overzicht

--- project/thuisbioscoop/db/overzicht.py
+++ project/thuisbioscoop/db/overzicht.py
@@ -1,12 +1,21 @@
 import sqlobject
+import datetime
+
 from .connection import conn
 from sqlobject import AND, OR
 
-def total_users(users):
+def total_users(users, valid_users):
+    valid_key(expire_date > current_date)
     users =
+    valid_users = users and valid_key(expire_date > current_date)
 
 def users_film():
     users =
 
-def films_aangeboden():
-    aanbieders =
+def supplier_films():
+    suppliers =
+
+def valid_key(expire_date, current_date):
+    expire_date =
+    current_date = {datetime.datetime.now():%d-%m-%Y}
+    if expire_date > current_date:
