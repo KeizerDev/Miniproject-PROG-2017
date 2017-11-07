ae89cc6fb0ef41ad7fae563632b45cde89935d62
Robert-Jan Keizer <robertjankeizer@gmail.com>
2017-11-07 12:26:50 +0100

    Added overview screen

--- project/main.py
+++ project/main.py
@@ -1,24 +1,24 @@
 import tkinter as tk
 
 
-class ScreenIntro():
+class ScreenIntro:
     def __init__(self, master):
         self.master = master
         self.frame_start = tk.Frame(master, background="#AA0203")
-        self.welkom = tk.Label(self.frame_start, text="Welkom, maak uw keuze:", foreground="white",
-                               background="#AA0203", height=5, font=10)
-        self.button1 = tk.Button(self.frame_start, text="Ik ben aanbieder", height=3, width=25,
-                                 command=self.show_screen_supplier)
-        self.button2 = tk.Button(self.frame_start, text="Ik ben bezoeker", height=3, width=25,
-                                 command=self.show_screen_visitor)
-        self.button3 = tk.Button(self.frame_start, text="Ik wil publieke informatie zien", height=3, width=25,
-                                 command=self.show_screen_public)
-
+        self.label_welcome = tk.Label(self.frame_start, text="Welkom, maak uw keuze:", foreground="white",
+                                      background="#AA0203", height=5, font=10)
+        self.button_supplier = tk.Button(self.frame_start, text="Ik ben aanbieder", height=3, width=25,
+                                         command=self.show_screen_supplier)
+        self.button_visitor = tk.Button(self.frame_start, text="Ik ben bezoeker", height=3, width=25,
+                                        command=self.show_screen_visitor)
+        self.button_public = tk.Button(self.frame_start, text="Ik wil publieke informatie zien", height=3, width=25,
+                                       command=self.show_screen_public)
         self.frame_start.pack(fill="both", expand=True)
-        self.welkom.pack()
-        self.button1.pack()
-        self.button2.pack()
-        self.button3.pack()
+
+        self.label_welcome.pack()
+        self.button_supplier.pack()
+        self.button_visitor.pack()
+        self.button_public.pack()
 
     def show_screen_supplier(self):
         self.frame_start.pack_forget()
@@ -33,25 +33,30 @@ class ScreenIntro():
         ScreenPublic(self.master)
 
 
-class ScreenSupplier():
+class ScreenSupplier:
     def __init__(self, master):
         self.master = master
         self.frame_supplier = tk.Frame(self.master, background="blue")
         self.frame_supplier.pack(fill="both", expand=True)
 
 
-class ScreenVisitor():
+class ScreenVisitor:
     def __init__(self, master):
         self.master = master
         self.frame_visitor = tk.Frame(self.master, background="green")
         self.frame_visitor.pack(fill="both", expand=True)
 
 
-class ScreenPublic():
+class ScreenPublic:
     def __init__(self, master):
         self.master = master
         self.frame_public = tk.Frame(self.master, background="black")
         self.frame_public.pack(fill="both", expand=True)
+
+
+class ScreenOverview:
+    def __init__(self, master):
+        
 
 
 class MainApplication(tk.Frame):
