65f9d651ca342e605bea1567a1acfa20075eb9d7
Steven <steven.toet@student.hu.nl>
2017-11-07 12:28:12 +0100

    Begin overzicht

---
+++ project/thuisbioscoop/db/overzicht.py
@@ -0,0 +1,12 @@
+import sqlobject
+from .connection import conn
+from sqlobject import AND, OR
+
+def total_users(users):
+    users =
+
+def users_film():
+    users =
+
+def films_aangeboden():
+    aanbieders =
