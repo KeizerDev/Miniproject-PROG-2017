79a15d365c3039774976761775af3fab9b5ff239
Steven <steven.toet@student.hu.nl>
2017-11-08 11:40:23 +0100

    overzicht

--- project/thuisbioscoop/db/overzicht.py
+++ project/thuisbioscoop/db/overzicht.py
@@ -16,6 +16,8 @@ def supplier_films():
     suppliers =
 
 def valid_key(expire_date, current_date):
-    expire_date =
+    date_1 = broadcast_time.select(ft_starttime.q.)
+    date_2 =
+    expire_date = date_1 - date_2
     current_date = {datetime.datetime.now():%d-%m-%Y}
     if expire_date > current_date:
